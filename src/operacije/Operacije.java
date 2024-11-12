f30e7e1599eb08c1bf3be378da28541b7d9fde32
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-11-12 22:18:39 +0100

    zajednicki part 1

---
+++ src/operacije/Operacije.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package operacije;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class Operacije {
+
+    public static final int LOGIN = 1;
+}
