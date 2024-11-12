4639a4bb1d74e4ac0a105815a2d3613af071fa4e
Vld Mrk <vm20190346@student.fon.bg.ac.rs>
2024-11-12 21:05:58 +0100

    inicijalni komit

---
+++ src/model/User.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author vldmrk
+ */
+public class User implements Serializable {
+
+    private int userId;
+    private String username;
+    private String pass;
+
+    public User() {
+    }
+
+    public User(int userId, String username, String pass) {
+        this.userId = userId;
+        this.username = username;
+        this.pass = pass;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        return Objects.equals(this.pass, other.pass);
+    }
+
+}
